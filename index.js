f7eb6fddc76c6eb668c55bdecb32afa9ba74ef47
Isaí Dzul <isai_27_dzul@hotmail.com>
2022-07-28 11:32:10 -0500

    cambios en el diseño y codigo JS

--- index.js
+++ index.js
@@ -11,23 +11,22 @@ sr.reveal('.pokemon_mostrar', {
 const input_todo = document.getElementById('input_todo')
 const pokemon_button = document.getElementById('pokemon_indiv')
 
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0'
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=102&offset=0'
 
 const getData = () => {
-    fetch(url)
-        .then((response) => {
+    fetch(url) //hacemos la peticion
+        .then((response) => { // en la respuesta retornamos la petición
             return response.json()
         })
         .then((data) => {
             data.results.forEach(element => {
                 fetch(element.url)
                     .then((response) => {
-                        
                         return response.json()
                             .then((response) => {
                                 console.log(response);
                                 const card = document.createElement("div");
-                                card.classList.add("col-md-2", "col-sm-4", "d-flex", "flex-column", "tarjetas");
+                                card.classList.add("col-md-2", "d-flex", "flex-column", "tarjetas");
                                 card.innerHTML = `
                                 <img class="imgPok" src="${response.sprites.front_default}">
                                 <h5 class="datosPok"> ID: ${response.id}</h5>
@@ -46,7 +45,22 @@ const getData = () => {
         })
 }
 
+const getRandomAsideWhenLoad = () =>{
+    fetch(url)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let arrayRandom = Math.floor(Math.random() * data.results.length);
+        buscarPokemon(data.results[arrayRandom].name);
+    })
+    .catch((error) =>{
+        console.error(error);
+    })
+}
+
 getData()
+getRandomAsideWhenLoad()
 
 const entrada = document.getElementById("input_todo");
 
@@ -68,12 +82,14 @@ function buscar(pokemon) {
     const elementoPadre = document.getElementById("elementoPadre");
     elementoPadre.innerHTML = "";
     const elementoHijo = document.createElement("div");
-    elementoHijo.classList.add("d-flex", "flex-column", "mx-auto", "justify-content-center", "align-items-center");
+    elementoHijo.classList.add("d-flex", "flex-column", "mx-auto", "justify-content-center", "align-items-center", "pokemonAside");
     
     elementoHijo.innerHTML = `
     <img class="imgAside" src="${pokemon.sprites.front_default}">
-    <h4 class="datosPok"> HEIGHT: ${pokemon.height}</h4>
-    <h4 class="datosPok"> WEIGHT: ${pokemon.weight}</h4>
+    <h5 class="datosPok"> Name: ${pokemon.name}</h5>
+    <h6 class="datosPok"> ABILITIES: ${pokemon.abilities[0].ability.name}</h6>
+    <h6 class="datosPok"> WEIGHT: ${pokemon.weight}</h6>
+    <h6 class="datosPok"> TYPES: ${pokemon.types[0].type.name}</h6>
     `;
   elementoPadre.appendChild(elementoHijo);
 
